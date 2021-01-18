e3635782093c27689a7b43af5d8651631a2d2257
ashinlaurel <ashinlaurel1999@gmail.com>
2021-01-19 03:41:18 +0530

    build

---
+++ FrontEnd/build/static/js/52.7f2cf70d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwindmill-dashboard-react"]=this["webpackJsonpwindmill-dashboard-react"]||[]).push([[52],{1042:function(a,e,n){"use strict";n.r(e);var t=n(0),l=n.n(t),r=n(86);e.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.a,null,"Blank"))}}}]);
+//# sourceMappingURL=52.7f2cf70d.chunk.js.map
