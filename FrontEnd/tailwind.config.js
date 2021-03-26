3e4843a84fc6de4a88767e8526ac80e4ff9532ea
Alan <alantomjose@gmail.com>
2021-03-26 20:34:14 +0530

    initial website, some broken

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -11,6 +11,104 @@ module.exports = windmill({
       boxShadow: {
         bottom: '0 5px 6px -7px rgba(0, 0, 0, 0.6), 0 2px 4px -5px rgba(0, 0, 0, 0.06)',
       },
+      minWidth: {
+        '10': '2.5rem',
+        '48': '12rem',
+      },
+      opacity: {
+        '90': '0.9',
+      },
+      scale: {
+        '98': '.98'
+      },
+      animation: {
+        float: 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)', },
+          '50%': { transform: 'translateY(-5%)', },
+        },
+      },
+      inset: {
+        '1/2': '50%',
+        'full': '100%',
+      },
+      letterSpacing: {
+        tighter: '-0.02em',
+        tight: '-0.01em',
+        normal: '0',
+        wide: '0.01em',
+        wider: '0.02em',
+        widest: '0.4em',
+      },
+      lineHeight: {
+        none: '1',
+        tighter: '1.125',
+        tight: '1.25',
+        snug: '1.375',
+        normal: '1.5',
+        relaxed: '1.625',
+        loose: '2',
+        '3': '.75rem',
+        '4': '1rem',
+        '5': '1.2rem',
+        '6': '1.5rem',
+        '7': '1.75rem',
+        '8': '2rem',
+        '9': '2.25rem',
+        '10': '2.5rem',
+      },
+
+      customForms: theme => ({
+        default: {
+          'input, textarea, multiselect, select, checkbox, radio': {
+            backgroundColor: theme('colors.white'),
+            borderColor: theme('colors.gray.300'),
+            borderRadius: theme('borderRadius.default'),
+            '&:focus': {
+              outline: undefined,
+              boxShadow: undefined,
+              borderColor: theme('colors.gray.500'),
+            },
+          },
+          'input, textarea, multiselect, select': {
+            backgroundColor: theme('colors.white'),
+            fontSize: undefined,
+            lineHeight: undefined,
+            paddingTop: theme('spacing.3'),
+            paddingRight: theme('spacing.4'),
+            paddingBottom: theme('spacing.3'),
+            paddingLeft: theme('spacing.4'),
+          },
+          'input, textarea': {
+            '&::placeholder': {
+              color: theme('colors.gray.500'),
+            },
+          },
+          select: {
+            paddingRight: theme('spacing.10'),
+            iconColor: theme('colors.gray.400'),
+          },
+          'checkbox, radio': {
+            color: theme('colors.gray.800'),
+            backgroundColor: theme('colors.white'),
+            borderRadius: theme('borderRadius.sm'),
+          },
+        },
+      }),
+    
+      
     },
   },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    textColor: ['responsive', 'hover', 'focus', 'group-hover'],
+    translate: ['responsive', 'hover', 'focus', 'group-hover'],
+    boxShadow: ['responsive', 'hover', 'focus', 'focus-within'],
+    opacity: ['responsive', 'hover', 'focus', 'group-hover'],
+  },
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ],
 })
