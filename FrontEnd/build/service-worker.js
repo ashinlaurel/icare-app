ac02e6d20b1db112cf87ca7c53228b0d77198f14
Alan <alantomjose@gmail.com>
2021-03-28 18:50:04 +0530

    28 march build

--- FrontEnd/build/service-worker.js
+++ FrontEnd/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.766b6cf3241f6edf429ed010b8a52805.js"
+  "/precache-manifest.aee76207d198e09eac5e95c0dff00c26.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
--- FrontEnd/build/static/js/57.72ec8948.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwindmill-dashboard-react"]=this["webpackJsonpwindmill-dashboard-react"]||[]).push([[57],{1061:function(a,e,n){"use strict";n.r(e);var t=n(0),l=n.n(t),r=n(97);e.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.a,null,"Blank"))}}}]);
-//# sourceMappingURL=57.72ec8948.chunk.js.map
