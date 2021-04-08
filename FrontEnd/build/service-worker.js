d689b0905c150444d89e572a7d63c44726b18865
ashinlaurel <ashinlaurel1999@gmail.com>
2021-04-09 02:05:51 +0530

    new build

--- FrontEnd/build/service-worker.js
+++ FrontEnd/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7ae0481d12f0496cfb36d177470ecf63.js"
+  "/precache-manifest.e9662b9e8740028d4d14814a2f165c42.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
