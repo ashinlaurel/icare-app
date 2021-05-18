20db7a4f3372d14b7be6ca9f2d0e05424cd53004
ashinlaurel <ashinlaurel1999@gmail.com>
2021-05-19 02:11:14 +0530

    cloud readiness

--- FrontEnd/src/LandingPages/Solutions/CloudReadiness.js
+++ FrontEnd/src/LandingPages/Solutions/CloudReadiness.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../../partials/Footer";
 import NewHero from "../../partials/NewHero";
 import herobg from "../../images/herobg.jpg";
+import cloudimage from "../../images/cloud-upload-icon-line-connection-circuit-board.jpg";
 
 const CloudReadiness = () => {
   return (
@@ -9,29 +10,32 @@ const CloudReadiness = () => {
       <div class="relative bg-white overflow-hidden  ">
         <div class=" max-h-xl ">
           <img
-            class="h-40 w-full  object-cover sm:h-72 md:h-56 lg:w-full lg:h-h-151"
-            src={
-              "https://cdn.pixabay.com/photo/2017/01/22/22/11/cloud-computing-2001090_960_720.jpg"
-            }
+            class="h-40 w-full object-cover object-left sm:h-72 md:h-56 lg:w-full lg:h-h-151"
+            src={cloudimage}
             alt=""
           />
         </div>
       </div>
 
       {/* Heading */}
-      <div className="  bg-hero-bg -mt-48 flex flex-col items-center justify-center">
-        <div class=" justify-center items-center max-w-sm md:max-w-3xl z-10 pb-8 bg-white    ">
+      <div className="  bg-hero-bg -mt-56 flex flex-col items-left justify-center">
+        <div class=" justify-center items-center max-w-sm md:max-w-xl z-10 pb-4 bg-black bg-opacity-25 ml-10">
           <main class="mt-4 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-5 lg:mt-0 lg:px-8 flex flex-col items-center justify-center ">
-            <div class="sm:text-center lg:text-left pt-10 ">
-              <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span class="block xl:inline">Cloud Solutions</span>
+            <div class="sm:text-center lg:text-left pt-4 ">
+              <h1 class="text-2xl tracking-tight  text-white sm:text-5xl md:text-3xl">
+                <span class="block xl:inline">Cloud Readiness Assessment</span>
+              </h1>
+              <h1 class="text-2xl tracking-tight  text-white sm:text-5xl md:text-lg">
+                <span class="block xl:inline">
+                  We ​evaluate and help you with your cloud roadmap
+                </span>
               </h1>
 
-              <h2>
-                <span class="block text-blue-700 text-4xl tracking-tight font-extrabold sm:text-5xl md:text-4xl ">
+              {/* <h2> */}
+              {/* <span class="block text-blue-700 text-4xl tracking-tight font-extrabold sm:text-5xl md:text-4xl ">
                   IT and Consulting Services
-                </span>
-              </h2>
+                </span> */}
+              {/* </h2> */}
             </div>
           </main>
         </div>
