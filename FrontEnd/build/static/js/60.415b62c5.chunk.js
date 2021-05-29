f630b3b08435e200319e066176bafb3a773d7ce8
Alan Tom Jose <alantomjose@gmail.com>
2021-05-29 18:45:51 +0530

    lst pdf

--- FrontEnd/build/static/js/60.415b62c5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpwindmill-dashboard-react"]=this["webpackJsonpwindmill-dashboard-react"]||[]).push([[60],{1152:function(a,e,n){"use strict";n.r(e);var t=n(0),l=n.n(t),r=n(190);e.default=function(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(r.a,null,"Blank"))}}}]);
-//# sourceMappingURL=60.415b62c5.chunk.js.map
