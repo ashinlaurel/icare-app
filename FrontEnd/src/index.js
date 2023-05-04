57aa0b7100aa5a59e220b523bc865a71518d226f
sidharth299 <sidharth2909@gmail.com>
2023-05-05 01:03:33 +0530

    Add sentry for error monitoring

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -9,6 +9,17 @@ import { TopBarProvider } from "./context/TopBarContext";
 import ThemedSuspense from "./components/ThemedSuspense";
 import { Windmill } from "@windmill/react-ui";
 import * as serviceWorker from "./serviceWorker";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://e349eba4793248019fc3b7ae9384d46f@o4505127139672064.ingest.sentry.io/4505127145308160",
+  integrations: [new Sentry.BrowserTracing(), new Sentry.Replay()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 // if (process.env.NODE_ENV !== 'production') {
 //   const axe = require('react-axe')
